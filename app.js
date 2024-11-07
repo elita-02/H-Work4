04c38a9e6158df3267fb4bf712afb617811e01ba
elita-02 <jyldyzbekovna04@outlook.com>
2024-11-08 00:05:18 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+
+// const gradus = +prompt('Аба ырайыны текшериниз');
+// if(gradus <=20){
+//     alert("Кун суук");
+// }
+// else if( gradus >=21 && gradus <=39 ){
+//     alert("Кун жылуу ");
+// }
+//     else if( gradus > 40 ){
+//     alert("Кун абдан ыссык");
+// }  
+//  else {
+//     alert("error");
+// }
+
+// const can =+prompt('чётный не чётный пороверим')
+// if(can==0){
+//     alert("чётный");
+//  }
+//  else if (can==1){
+//     alert("не чётный");
+//  }
+// else if(can==2){
+//     alert("чётный");
+// }
+// else if(can==3){
+//     alert("не чётный");
+// }
+// else if(can==4){
+//     alert("чётный");
+// }
+// else if(can==5){
+//     alert("не чётный");
+// }
+// else if(can==6){
+//     alert("чётный");
+// }
+// else if(can==7){
+//     alert("не чётный");
+// }
+// else if(can==8){
+//     alert("чётный");
+// }
+// else if(can==9){
+//     alert("не чётный");
+// }
+// else if(can==10){
+//     alert("чётный");
+// }
+// else {
+//     alert("error");
+// }
+
+const seosan=+prompt("4 мезгил кайсы айларда жайгашкан ")
+switch (seosan) {
+    case 12:
+        alert("Кыш мезгили Декабрь айы ")
+        break;
+    case 1:
+        alert("Кыш мезгили Январь айы")
+        break;
+    case 2:
+        alert("Кыш мезгили Февраль айы")
+        break;
+    case 3:
+        alert("Жаз мезгили Март айы")
+        break;
+    case 4:
+        alert("Жаз мезгили Апрель айы")
+        break;
+    case 5:
+        alert("Жаз мезгили Май айы")
+        break;
+    case 6:
+        alert("Жай мезгили Июнь айы")
+        break;
+    case 7:
+        alert("Жай мезгили Июль айы")
+        break;
+    case 8:
+        alert("Жай мезгили Августь айы")
+        break;
+    case 9:
+        alert("Куз мезгили Сентябрь айы")
+        break;
+    case 10:
+        alert("Куз мезгили Октярбрь айы")
+        break;
+    case 11:
+        alert("Куз мезгили Ноябрь айы")
+        break;
+    default:
+        alert("error")
+}
